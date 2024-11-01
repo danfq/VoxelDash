e3b96312c715ea80f0849dfedc6f141ce2319b47
DanFQ <danfq.dev@gmail.com>
2024-11-01 00:34:45 +0000

    Base (Initial Commit)

---
+++ android/app/src/main/kotlin/dev/danfq/voxeldash/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.danfq.voxeldash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(
+    AdaptiveTheme(
+      light: light,
+      dark: dark,
+      initial: AdaptiveThemeMode.system,
+      builder: (light, dark) {
+        return GetMaterialApp(
+          theme: light,
+          darkTheme: dark,
+          home: ,
+        );
+      },
+    ),
+  );
+}
